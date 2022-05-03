ed0b2a5515f69ab4c219a3721b40b2d13c0ed088
alina-yanchuk02 <alina-yanchuk02@outlook.pt>
2022-05-03 15:13:57 +0100

    update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://reduction-admin.github.io/react-reduction/precache-manifest.dbbee591e450e46f958ced80d75abbcb.js"
+  "https://alina-yanchuk02.github.io/estigma/precache-manifest.994bd6e41563787749ba2360d7655cc9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://reduction-admin.github.io/react-reduction/index.html", {
+workbox.routing.registerNavigationRoute("https://alina-yanchuk02.github.io/estigma/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
